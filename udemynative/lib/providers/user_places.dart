917c3168bf6c0577a3252f43388bfd90f08b13d7
João Henrique <joao.calasans@inlog.com.br>
2024-04-21 11:29:12 -0300

    primeiro commit

---
+++ udemynative/lib/providers/user_places.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:udemynative/models/place.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []); // deve ser iniciado com constante
+
+  void addPlace(String title, File image) {
+    final newPlace = Place(title: title, image: image);
+
+    state = [
+      newPlace,
+      ...state
+    ]; //criar uma nova lista contendo a lista anterior(state) e adiciona no inicio o novo elemento
+  }
+}
+
+final userPlacesProvider =
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+        (ref) => UserPlacesNotifier());
